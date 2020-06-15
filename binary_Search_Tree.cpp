78aae86ebb3a69d279e610bc84d8059ea09ad990
Park Seungri <21900282@handong.edu>
2020-06-15 16:29:55 +0900

    Update binary_Search_Tree.cpp - stack

--- binary_Search_Tree.cpp
+++ binary_Search_Tree.cpp
@@ -17,3 +17,32 @@ void bst_node :: set_data(string s1, string s2, string s3){
     phonenum = s2;
     birth = s3;
 }
+
+class nstack{
+    bst_node *t[SIZE];
+    int top;
+public :
+    nstack();
+    void push(bst_node *s);
+    bst_node* pop();
+    bool stack_empty();
+};
+
+nstack :: nstack(){
+    top = 0;
+}
+
+void nstack :: push(bst_node *s){
+    t[top] = s;
+    top++;
+}
+
+bst_node* nstack :: pop(){
+    top--;
+    return t[top];
+}
+
+bool nstack :: stack_empty(){
+    if(top == 0) return true;
+    else return false;
+}
